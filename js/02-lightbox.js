38abfeaf811b98a66e4277882eb30ca0a0cc3595
VitaliiPankin <niknap777@ukr.net>
2022-02-13 18:08:24 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", renderGallery(galleryItems));
+
+function renderGallery(elemensGallery) {
+  const makeUp = elemensGallery
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}"  />
+     </a>`
+    )
+    .join("");
+
+  return makeUp;
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
