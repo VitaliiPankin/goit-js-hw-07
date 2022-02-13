2e762ea9edd9bf667121fa69bcf2501aee9ddf87
VitaliiPankin <niknap777@ukr.net>
2022-02-13 15:12:18 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML("beforeend", renderGallery(galleryItems));
+galleryRef.addEventListener("click", openImage);
+
+function renderGallery(gallery) {
+  const makeUp = gallery
+    .map(
+      (gallery) => `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${gallery.original}">
+    <img class="gallery__image"
+    src="${gallery.preview}"
+    data-source="${gallery.original}"
+    alt="${gallery.description}">
+    </a></div>
+    `
+    )
+    .join("");
+
+  return makeUp;
+}
+
+// 2. Реализация делегирования на div.gallery и получение
+// url большого изображения.
+
+function openImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+            <img src="${event.target.dataset.source}" />
+        `);
+  console.dir(event.target.dataset.source);
+  instance.show();
+}
